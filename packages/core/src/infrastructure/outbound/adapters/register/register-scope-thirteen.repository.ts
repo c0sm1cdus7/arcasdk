c5d7dd256eec5258e522f84da1c8ba69d751082c
Franco Altuna <franco.altuna@me.com>
2026-03-09 16:31:19 -0300

    Fix

--- packages/core/src/infrastructure/outbound/adapters/register/register-scope-thirteen.repository.ts
+++ packages/core/src/infrastructure/outbound/adapters/register/register-scope-thirteen.repository.ts
@@ -69,7 +69,7 @@ export class RegisterScopeThirteenRepository extends BaseSoapRepository implemen
             } as any);
 
             const personaReturn = output.personaReturn;
-            if (!personaReturn) {
+            if (!personaReturn || (!personaReturn.persona && !(personaReturn as TaxpayerDetailsDto).tipoPersona)) {
                 return null;
             }
 
